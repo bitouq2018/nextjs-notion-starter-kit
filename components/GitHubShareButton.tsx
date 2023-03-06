7b80bfc8850dda61ce00d17f10b2a1a30a5d78ee
bitouq2018 <42560916+bitouq2018@users.noreply.github.com>
2023-03-06 21:53:40 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,11 +1,11 @@
-/*import * as React from 'react'
+import * as React from 'react'
 
 import styles from './styles.module.css'
 
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://bitouq.com'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
@@ -45,4 +45,4 @@ export const GitHubShareButton: React.FC = () => {
       </svg>
     </a>
   )
-} */
+} 
